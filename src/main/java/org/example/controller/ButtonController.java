5d14fc4eaf328f9d62a4b6c6342de4698861060e
SarangaKalhari <kalharisaranga9@gmail.com>
2026-02-14 23:34:45 +0530

    Create API to retrieve category button list for UI

--- src/main/java/org/example/controller/ButtonController.java
+++ src/main/java/org/example/controller/ButtonController.java
@@ -4,6 +4,7 @@ import org.example.model.dto.CategoryDTO;
 import org.example.model.entity.Category;
 import org.example.service.ButtonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/buttons")
+@CrossOrigin("http://localhost:4200/")
 public class ButtonController {
 
     @Autowired
