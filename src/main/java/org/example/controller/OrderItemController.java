57d41c295d816d27e32325a20040467ac80ab1bb
SarangaKalhari <kalharisaranga9@gmail.com>
2026-03-16 23:55:07 +0530

    feat(sales): implement get top order item

--- src/main/java/org/example/controller/OrderItemController.java
+++ src/main/java/org/example/controller/OrderItemController.java
@@ -32,10 +32,10 @@ public class OrderItemController {
         return orderIemService.getMonthlyTop10();
     }
 
-//    @GetMapping("/top-1/daily")
-//    public List<TopSellingItemDTO> dailyTop(){
-//        return orderIemService.getTopSellingItemsWithTie();
-//    }
+    @GetMapping("/top-1/daily")
+    public List<TopSellingItemDTO> dailyTop(){
+        return orderIemService.dailyTop();
+    }
 
     @GetMapping("/top-1/weekly")
     public List<TopSellingItemDTO> weeklyTop(){
